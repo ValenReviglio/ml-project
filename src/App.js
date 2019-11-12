0c5d34c727badc871d0fe489421f4fde55bb8e4e
Valentin Reviglio <valentin.reviglio@mercadolibre.com>
2019-11-12 11:25:38 -0300

    Version ready for demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const React = require('react');
+const logo = require('./logo.svg');
+// const BadgeIcon = require('@ml-demo/badgeicon');
+// const { Truck } = require('@ml-demo/common');
+require('./App.css');
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        { /*
+          <div className="icon-container">
+            <BadgeIcon square={'150px'} icon={Truck} />
+          </div>
+        */ }
+        <p className="App-link">
+          This is a test of the <strong>@ml-demo</strong> library.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
